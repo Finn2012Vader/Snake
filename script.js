4e343ad29e7a27fdbd2c055fe9f71a2e5f77c376
Finn2012Vader <tymofiy2012m@gmail.com>
2025-05-25 11:57:33 +0300

    Move snake, spawn food.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let gameState = 'game';
 let headX = 0, headY = 0;
 let moveX = 0, moveY = 0;
-
+let foodX = Math.floor(Math.random()*20)*20;
+let foodY = Math.floor(Math.random()*20)*20;
+let tail = []
 function setup() {
 createCanvas(400, 400);
 }
@@ -10,15 +12,97 @@ function draw() {
   if (gameState == 'game')
 {
   background(0, 0, 0);
-  fill(255,255,255)
+  fill(0,255,0)
   stroke(0);
   rect(headX, headY, 20, 20);
   
+  fill(255,0,0)
+  rect(foodX, foodY, 20, 20);
+  if(headX == foodX && headY == foodY)
+    {
+     foodX = Math.floor(Math.random()*20)*20;
+     foodY = Math.floor(Math.random()*20)*20;
+      add_tail();
+    }
+  
   if (frameCount % 5 == 0){
+    update_tail(headX, headY);
     headX += moveX;
     headY += moveY;
+    if (headX >= 400)
+      {
+        headX = 0;
+      }
+    else if (headX <= -20)
+      {
+        headX = 380;
+      }
+      if (headY >= 400)
+      {
+        headY = 0;
+      }
+     else if (headY <= -20)
+      {
+        headY = 380;
+      }
+  
   }
+  
   
 }
   
+}
+
+
+
+
+function keyPressed()
+{
+  if (keyCode == 83 && moveY != -20)// S
+    {
+    moveX = 0;
+    moveY = 20;
+    }
+  
+  else if (keyCode == 87 && moveY != 20)//W
+    {
+    moveX = 0;
+    moveY = -20;
+    }
+  
+  else if (keyCode == 65 && moveX != 20)//A
+    {
+    moveX = -20;
+    moveY = 0;
+    }
+    
+  else if (keyCode == 68 && moveX != -20)//D
+    {
+    moveX = 20;
+    moveY = 0;
+    }
+      
+
+  
+  
+}
+function add_tail()
+{
+  tail.push({
+    x: headX,
+    y: headY
+  })
+}
+function update_tail(targetX, targetY)
+{
+  if (tail.length > 0)
+    {
+      for (let i = 0; i < tail.length()-1; i++)
+        {
+          tail[i].x = tail[i-1].x
+          tail[1].y = tail[i-1].y
+        }
+      tail[0].x = targetX;
+      tail[0].y = targetY;
+    }
 }
